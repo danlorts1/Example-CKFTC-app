bb13900bfb503f689665f0ac3fb8f3b0630cf836
Dan Lorts <dlorts@cranbrook.edu>
2016-12-26 11:53:06 -0500

    add 5th motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicTeleop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by rstudent on 12/25/2016.
@@ -10,13 +11,18 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Holonomic")
 public class HolonomicTeleop extends OpMode {
     MecanumDrivebase b;
+    DcMotor m;
+
     @Override
     public void init() {
         b=new MecanumDrivebase(hardwareMap);
+        m=hardwareMap.dcMotor.get("m5");
+
 
     }
     @Override
     public void loop() {
-        b.driveHolonomic(gamepad1.left_stick_x, -gamepad1.right_stick_y, gamepad1.right_stick_x);
+        b.driveHolonomic(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+        m.setPower(gamepad1.right_trigger);
     }
 }
